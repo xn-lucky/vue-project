31a9e20604db875673561e1741b5e4e5dc156db7
3D1Y <hi_xieyufeng@163.com>
2021-01-04 21:33:10 +0800

    头部组件

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,38 +1,75 @@
 <template>
-  <div class="header">
-    <h1 class="logo">
-      <a href="">
-        <img src="@assets/images/ia_100000033.png" alt="" />
-      </a>
-    </h1>
-    <div class="search">
-      <div class="input">
-        <input type="text" placeholder="想写点什么呀" />
-        <div class="btn">
-          <i class="icon"></i>
-          <!-- <span>搜索</span> -->
+  <div>
+    <div class="header">
+      <h1 class="logo">
+        <a href="">
+          <img src="@assets/images/ia_100000033.png" alt="" />
+        </a>
+      </h1>
+      <div class="search">
+        <div class="input">
+          <input type="text" placeholder="想写点什么呀" />
+          <div class="btn">
+            <i class="iconfont icon-search"></i>
+            <!-- <span>搜索</span> -->
+          </div>
+        </div>
+      </div>
+      <div class="topNav">
+        <ul class="bsUl">
+          <li>
+            <a target="_blank" href="#">客服中心</a>
+          </li>
+          <li>
+            <a class="" target="_blank" href="#">招贤纳士</a>
+          </li>
+          <li><a target="_blank" href="#">会员中心 </a></li>
+          <li>
+            <a target="_blank" href="#">商务合作</a>
+          </li>
+        </ul>
+        <div class="login_area">
+          <div id="login_in" class="clearfix">
+            <a class="login_btn" id="login_btn">登录</a>
+            <!-- <a target="_blank" href="#" class="regin_btn" id="regin_btn">注册</a> -->
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="topNav">
-      <ul class="bsUl">
-        <li>
-          <a target="_blank" href="#">客服中心</a>
-        </li>
-        <li>
-          <a class="" target="_blank" href="#">招贤纳士</a>
-        </li>
-        <li><a target="_blank" href="#">会员中心 </a></li>
-        <li>
-          <a target="_blank" href="#">商务合作</a>
-        </li>
-      </ul>
-      <div class="login_area">
-        <div id="login_in" class="clearfix">
-          <a class="login_btn" id="login_btn">登录</a>
-          <!-- <a target="_blank" href="#" class="regin_btn" id="regin_btn">注册</a> -->
-        </div>
+    <div class="navWrap">
+      <div class="nav">
+        <ul class="homeNav">
+          <li><a href="#">首页</a></li>
+          <li><a href="#">榜单</a></li>
+          <li><a href="#">下载客户端</a></li>
+          <li><a href="#">更多</a></li>
+        </ul>
+        <!-- <ul class="secondMenu">
+          <li><a href="#">电台</a></li>
+          <li><a href="#">MV</a></li>
+          <li><a href="#">歌单</a></li>
+          <li><a href="#">歌手</a></li>
+        </ul> -->
+        <ul class="subNav">
+          <li>
+            <a href="#"><i class="iconfont icon-shoucang"></i>直播</a>
+          </li>
+          <li>
+            <a href="#"><i class="iconfont icon-shangcheng"></i>商城</a>
+          </li>
+          <li>
+            <a href="#"><i class="iconfont icon-erji"></i>音乐人</a>
+          </li>
+          <li>
+            <a href="#"><i class="iconfont icon-fangzicopy"></i>代理商</a>
+          </li>
+          <li>
+            <a href="#"><i class="iconfont icon-maikefeng"></i>主播电台</a>
+          </li>
+          <li>
+            <a href="#"><i class="iconfont icon-kaifangpingtai"></i>开放平台</a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -73,6 +110,17 @@ export default {
   color: #666;
   outline: none;
 }
+.header .search .input .btn {
+  // display: block;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 34px;
+  height: 34px;
+  text-align: center;
+  margin: 0 auto;
+  line-height: 34px;
+}
 .header .topNav {
   display: flex;
   align-items: center;
@@ -105,5 +153,57 @@ export default {
   color: #fff;
   cursor: pointer;
   background-color: #169af3;
+}
+.navWrap {
+  position: relative;
+  z-index: 999;
+  width: 100%;
+  height: 55px;
+  background-color: #2c323b;
+}
+.navWrap .nav {
+  width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+.navWrap .nav .homeNav {
+  display: flex;
+}
+.navWrap .nav .homeNav li {
+  line-height: 55px;
+  font-size: 16px;
+  padding: 0 30px;
+}
+.navWrap .nav .homeNav li a {
+  color: #ddd;
+}
+.navWrap .nav .homeNav li:hover {
+  background-color: #0c8ed9;
+  color: #fff;
+}
+.navWrap .nav .homeNav li:hover a {
+  color: #fff !important;
+}
+.navWrap .nav .homeNav li a:hover {
+  text-decoration: none;
+}
+.navWrap .nav .subNav {
+  display: flex;
+  align-items: center;
+}
+.navWrap .nav .subNav li {
+  padding-left: 20px;
+  font-size: 13px;
+}
+.navWrap .nav .subNav li a {
+  color: #bbb;
+}
+.navWrap .nav .subNav li a:hover {
+  color: #14a9ff;
+  text-decoration: none;
+}
+.navWrap .nav .subNav li a i {
+  color: #fff;
 }
 </style>
