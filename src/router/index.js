e0c7281b3f5fbe2fa8475bb1b282a57fd466bc13
wujiajing <445476933@qq.com>
2021-01-08 11:16:26 +0800

    feat: 重写vueRouter的push和replace方法

--- src/router/index.js
+++ src/router/index.js
@@ -19,13 +19,37 @@ const SingerDetails = () =>
   import('@views/Singer/SingerDetails')
 const SongList = () =>
   import( /* webpackChunkName: "Home" */ "@views/SongList");
+
+// 重写push和replace方法
+// 目的：为了让编程式导航重复点击时不报错~
+const push = VueRouter.prototype.push;
+const replace = VueRouter.prototype.replace;
+
+VueRouter.prototype.push = function (location, onComplete, onAbort) {
+  // 如果用户想处理失败，就处理
+  if (onComplete && onAbort) {
+    return push.call(this, location, onComplete, onAbort);
+  }
+  // 如果用户不处理失败，给默认值：空函数
+  return push.call(this, location, onComplete, () => { });
+};
+
+VueRouter.prototype.replace = function (location, onComplete, onAbort) {
+  // 如果用户想处理失败，就处理
+  if (onComplete && onAbort) {
+    return replace.call(this, location, onComplete, onAbort);
+  }
+  // 如果用户不处理失败，给默认值：空函数
+  return replace.call(this, location, onComplete, () => { });
+};
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
 
   // 模式
   // mode: 'hash', // 默认模式
-  // mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
