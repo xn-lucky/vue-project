7d0d432baa76bebd256780f99144ce957d81af93
秦佳盛 <1337275169@qq.com>
2021-01-08 10:09:13 +0800

    feat:分享小功能

--- src/views/SongList/index.vue
+++ src/views/SongList/index.vue
@@ -65,21 +65,81 @@
                 </span></router-link
               >
               <a href="javascript:void(0)"
-                ><span
-                  ><i class="iconfont icon-fenxiang1"></i
-                  ><i @click="share">分享</i>
+                ><span @click="share"
+                  ><i class="iconfont icon-fenxiang1"></i><i>分享</i>
                 </span></a
               >
+              <el-dialog
+                title="分享歌曲"
+                :visible.sync="shareShow"
+                width="30%"
+                :before-close="handleClose2"
+              >
+                <div class="el-dialog-box-two">
+                  <a
+                    ><span
+                      class="el-dialog-box-two-span1"
+                      style="
+                        background: url( background: url(https://staticssl.kugou.com/public/root/images/share_weixin.png));
+                      "
+                    ></span
+                  ></a>
+
+                  <a
+                    target="_blank"
+                    href="https://connect.qq.com/widget/shareqq/index.html?url=https%3A%2F%2Ft1.kugou.com%2Fsong.html%3Fid%3D1nmRJ64xTV2&desc=&title=%E9%B9%BF%E6%99%97%20-%20%E6%80%9C%E5%9F%8E%E8%BE%9E&summary=%E6%88%91%E5%9C%A8%E9%85%B7%E7%8B%97%E5%B8%B8%E5%90%AC%E7%9A%84%E3%80%8A%E9%B9%BF%E6%99%97%20-%20%E6%80%9C%E5%9F%8E%E8%BE%9E%E3%80%8B%EF%BC%8C%E4%BD%A0%E4%B9%9F%E6%9D%A5%E5%90%AC%E5%90%AC%E5%90%A7%EF%BC%81%EF%BC%88%E6%9D%A5%E8%87%AA%20web%20%E9%85%B7%E7%8B%97%E9%9F%B3%E4%B9%90%EF%BC%89&pics=http://www.kugou.com/yy/static/images/share-cover.png&flash=&site=www.kugou.com"
+                    ><span class="el-dialog-box-two-span2"></span
+                  ></a>
+                  <a
+                    target="_blank"
+                    href="https://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=https%3A%2F%2Ft1.kugou.com%2Fsong.html%3Fid%3D1ntlWd5xTV2&title=%E6%9D%A8%E5%B0%8F%E5%A3%AE%20-%20%E7%83%9F%E9%9B%A8%E6%88%90%E6%80%9D&pics=http://www.kugou.com/yy/static/images/share-cover.png&summary=%E6%88%91%E5%9C%A8%E9%85%B7%E7%8B%97%E5%B8%B8%E5%90%AC%E7%9A%84%E3%80%8A%E6%9D%A8%E5%B0%8F%E5%A3%AE%20-%20%E7%83%9F%E9%9B%A8%E6%88%90%E6%80%9D%E3%80%8B%EF%BC%8C%E4%BD%A0%E4%B9%9F%E6%9D%A5%E5%90%AC%E5%90%AC%E5%90%A7%EF%BC%81%EF%BC%88%E6%9D%A5%E8%87%AA%20web%20%E9%85%B7%E7%8B%97%E9%9F%B3%E4%B9%90%EF%BC%89"
+                    ><span class="el-dialog-box-two-span3"></span
+                  ></a>
+                  <a
+                    target="_blank"
+                    href="https://service.weibo.com/share/share.php?appkey=340086183&pic=http://www.kugou.com/yy/static/images/share-cover.png&url=https%3A%2F%2Ft3.kugou.com%2Fsong.html%3Fid%3D1nuGf7bxTV2&title=%E6%88%91%E5%9C%A8%E9%85%B7%E7%8B%97%E5%B8%B8%E5%90%AC%E7%9A%84%E3%80%8A%E6%9D%A8%E5%B0%8F%E5%A3%AE%20-%20%E7%83%9F%E9%9B%A8%E6%88%90%E6%80%9D%E3%80%8B%EF%BC%8C%E4%BD%A0%E4%B9%9F%E6%9D%A5%E5%90%AC%E5%90%AC%E5%90%A7%EF%BC%81%EF%BC%88%E6%9D%A5%E8%87%AA%20web%20%E9%85%B7%E7%8B%97%E9%9F%B3%E4%B9%90%EF%BC%89#_loginLayer_1609935763639"
+                    ><span class="el-dialog-box-two-span4"></span
+                  ></a>
+                </div>
+              </el-dialog>
+              <!-- <el-dialog
+                title="分享歌曲"
+                :visible.sync="shareShow"
+                width="30%"
+                :before-close="handleClose2"
+                append-to-body
+              > -->
+              <!-- <div class="el-dialog-box-two">
+                  <span class="el-dialog-box-two-span1"></span>
+                  <a
+                    target="_blank"
+                    href="https://connect.qq.com/widget/shareqq/index.html?url=https%3A%2F%2Ft1.kugou.com%2Fsong.html%3Fid%3D1nmRJ64xTV2&desc=&title=%E9%B9%BF%E6%99%97%20-%20%E6%80%9C%E5%9F%8E%E8%BE%9E&summary=%E6%88%91%E5%9C%A8%E9%85%B7%E7%8B%97%E5%B8%B8%E5%90%AC%E7%9A%84%E3%80%8A%E9%B9%BF%E6%99%97%20-%20%E6%80%9C%E5%9F%8E%E8%BE%9E%E3%80%8B%EF%BC%8C%E4%BD%A0%E4%B9%9F%E6%9D%A5%E5%90%AC%E5%90%AC%E5%90%A7%EF%BC%81%EF%BC%88%E6%9D%A5%E8%87%AA%20web%20%E9%85%B7%E7%8B%97%E9%9F%B3%E4%B9%90%EF%BC%89&pics=http://www.kugou.com/yy/static/images/share-cover.png&flash=&site=www.kugou.com"
+                    ><span class="el-dialog-box-two-span2"></span
+                  ></a>
+                  <a
+                    target="_blank"
+                    href="https://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=https%3A%2F%2Ft1.kugou.com%2Fsong.html%3Fid%3D1ntlWd5xTV2&title=%E6%9D%A8%E5%B0%8F%E5%A3%AE%20-%20%E7%83%9F%E9%9B%A8%E6%88%90%E6%80%9D&pics=http://www.kugou.com/yy/static/images/share-cover.png&summary=%E6%88%91%E5%9C%A8%E9%85%B7%E7%8B%97%E5%B8%B8%E5%90%AC%E7%9A%84%E3%80%8A%E6%9D%A8%E5%B0%8F%E5%A3%AE%20-%20%E7%83%9F%E9%9B%A8%E6%88%90%E6%80%9D%E3%80%8B%EF%BC%8C%E4%BD%A0%E4%B9%9F%E6%9D%A5%E5%90%AC%E5%90%AC%E5%90%A7%EF%BC%81%EF%BC%88%E6%9D%A5%E8%87%AA%20web%20%E9%85%B7%E7%8B%97%E9%9F%B3%E4%B9%90%EF%BC%89"
+                    ><span class="el-dialog-box-two-span3"></span
+                  ></a>
+                  <a
+                    target="_blank"
+                    href="https://service.weibo.com/share/share.php?appkey=340086183&pic=http://www.kugou.com/yy/static/images/share-cover.png&url=https%3A%2F%2Ft3.kugou.com%2Fsong.html%3Fid%3D1nuGf7bxTV2&title=%E6%88%91%E5%9C%A8%E9%85%B7%E7%8B%97%E5%B8%B8%E5%90%AC%E7%9A%84%E3%80%8A%E6%9D%A8%E5%B0%8F%E5%A3%AE%20-%20%E7%83%9F%E9%9B%A8%E6%88%90%E6%80%9D%E3%80%8B%EF%BC%8C%E4%BD%A0%E4%B9%9F%E6%9D%A5%E5%90%AC%E5%90%AC%E5%90%A7%EF%BC%81%EF%BC%88%E6%9D%A5%E8%87%AA%20web%20%E9%85%B7%E7%8B%97%E9%9F%B3%E4%B9%90%EF%BC%89#_loginLayer_1609935763639"
+                    ><span class="el-dialog-box-two-span4"></span
+                  ></a>
+                </div> -->
+              <!-- <span slot="footer" class="dialog-footer">
+                      <el-button @click="dialogVisible = false"
+                        >取 消</el-button
+                      >
+                      <el-button type="primary" @click="dialogVisible = false"
+                        >确 定</el-button
+                      >
+                    </span> -->
+              <!-- </el-dialog> -->
             </div>
           </div>
         </li>
       </ul>
-    </div>
-    <div class="share">
-      <div class="header"><i>分享歌曲</i><i>×</i></div>
-      <div class="content">
-        <dl></dl>
-      </div>
     </div>
   </div>
 </template>
@@ -94,6 +154,7 @@ export default {
       currentIndex: 1,
       songList: [],
       result: {},
+      shareShow: false,
     };
   },
   created() {
@@ -101,6 +162,14 @@ export default {
   },
 
   methods: {
+    // 点击分享按钮
+    share() {
+      this.shareShow = true;
+    },
+    // 关闭分享对话框
+    handleClose2() {
+      this.shareShow = false;
+    },
     changeSong(index) {
       if (index === 1) {
         this.songList = this.result.songList1;
@@ -278,7 +347,53 @@ export default {
           }
           .btn {
             margin-top: 8px;
-
+            .el-dialog__body {
+              .el-dialog-box-two {
+                display: flex;
+                justify-content: space-around;
+                margin: 0 auto;
+                .el-dialog-box-two-span1 {
+                  width: 62px;
+                  height: 62px;
+                  display: block;
+                  cursor: pointer;
+                  background: url(https://staticssl.kugou.com/public/root/images/share_weixin.png)
+                    no-repeat left top;
+                  padding: 0;
+                  margin-right: 30px;
+                }
+                .el-dialog-box-two-span2 {
+                  width: 62px;
+                  height: 62px;
+                  display: block;
+                  cursor: pointer;
+                  background: url(https://staticssl.kugou.com/public/root/images/share_qq.png)
+                    no-repeat left top;
+                  padding: 0;
+                  margin-right: 30px;
+                }
+                .el-dialog-box-two-span3 {
+                  width: 62px;
+                  height: 62px;
+                  display: block;
+                  cursor: pointer;
+                  background: url(https://staticssl.kugou.com/public/root/images/share_qqspace.png)
+                    no-repeat left top;
+                  padding: 0;
+                  margin-right: 30px;
+                }
+                .el-dialog-box-two-span4 {
+                  width: 62px;
+                  height: 62px;
+                  display: block;
+                  cursor: pointer;
+                  background: url(https://staticssl.kugou.com/public/root/images/share_weibo.png)
+                    no-repeat left top;
+                  padding: 0;
+                  margin-right: 30px;
+                }
+              }
+            }
             a {
               text-decoration: none;
             }
@@ -288,6 +403,7 @@ export default {
               margin-right: 30px;
               text-align: center;
               color: white;
+
               &:hover {
                 background: #0b66a3;
               }
